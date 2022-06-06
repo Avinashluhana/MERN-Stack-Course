c08e8b77325e7fb0f40d23d97c1342a4fbf7e097
avinashluhana <aviluhana@gmail.com>
2022-06-06 20:46:57 +0500

    added body parser and requested data from postman

--- 2/index.js
+++ 2/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
 
 const myData = {
   name: "avinash",
   lastName: "Kumar",
 };
+
 app.get("/", function (req, res) {
   res.send(myData);
 });
+
 app.post("/", (req, res) => {
-  const data = myData;
+  const data = req.body;
   res.send(data);
+  console.log(data);
 });
+
 app.put("/", (req, res) => {
   myData.name = "Avinash";
   myData.lastName = "Luhana";
