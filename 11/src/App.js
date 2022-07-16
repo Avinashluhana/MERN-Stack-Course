4a77733c51a8646d74eecc414c190bec9eb68737
avinashluhana <aviluhana@gmail.com>
2022-07-16 18:16:39 +0500

    added react

---
+++ 11/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+
+// import "./styles.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hale payo</h1>
+    </div>
+  );
+}
+
+export default App;
